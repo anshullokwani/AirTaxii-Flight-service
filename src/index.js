72e733520956424cccf9fbdf5f0300449010c46d
Anshul Lokwani <lokwani99anshul@gmail.com>
2023-11-05 14:10:58 +0530

    added-basic-node-express-functionalities

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const {ServerConfig, Logger} = require('./config');
+const apiRoutes = require('./routes');
+
+const app = express();
+
+app.use('/api', apiRoutes);
+
+app.listen(ServerConfig.PORT, () => {
+    console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+})
