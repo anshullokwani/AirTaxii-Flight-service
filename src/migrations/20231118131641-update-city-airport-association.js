cca3e5d23bf865f080a5849fbebd2cde760b9991
Anshul Lokwani <lokwani99anshul@gmail.com>
2023-11-18 20:36:41 +0530

    foreign-key-association

---
+++ src/migrations/20231118131641-update-city-airport-association.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+    await queryInterface.addConstraint('Airports', {
+      type: 'foreign key',
+      name: 'city_fkey_constraint',
+      fields: ['cityId'],
+      references: {
+        table: 'Cities',
+        field: 'id'
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE'
+    });
+
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+
+    await queryInterface.removeConstraint('Airports', 'city_fkey_constraint')
+  }
+};
