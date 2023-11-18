42499987c24094f3d374423e539ee240375d0ce7
Anshul Lokwani <lokwani99anshul@gmail.com>
2023-11-18 23:47:14 +0530

    added-flights-model-with-foreign-key-mappings

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, async () => {
+app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-    const {City, Airport } = require('./models');
-    const city = await City.findByPk(1);
 })
