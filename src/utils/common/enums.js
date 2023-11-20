e162441e50b6b67aac959d47d5d96c4eb2c9c8b3
Anshul Lokwani <lokwani99anshul@gmail.com>
2023-11-20 13:40:11 +0530

    added-seat-model-and-associations

---
+++ src/utils/common/enums.js
@@ -0,0 +1,12 @@
+const SEAT_TYPE = {
+    BUSINESS: 'business',
+    ECONOMY: 'economy',
+    PREMIUM_ECONOMY: 'premium-economy',
+    FIRST_CLASS: 'first-class'
+}
+
+
+
+module.exports = {
+    SEAT_TYPE
+}
